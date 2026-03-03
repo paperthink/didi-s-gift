0bdcf236c5f549ada5f051e2202e9b391c46ca9f
paperthink <pattaboissxp2010@gmail.com>
2026-03-03 12:30:08 +0530

    gift_done

---
+++ sonakshi.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+
+void sonakshi(int age){
+    printf("How old are you?: :)\n");
+    printf("Yoo! Congratulations! ");
+    printf("%d", age);
+    printf(" didi hehhe");
+
+    
+    
+}
+
+int main(){
+    printf("Hello!\n");
+    sonakshi(17); // Helo didi, idk ur age, so put ur age in
+    //this parameters. line 13.
+    return 0;
+    
+
+}
+
+
+
